016d98548173a6dcdb2f0e37ba2f1600e5a06e89
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-02-24 19:31:40 -0600

    models and category routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
+router.use('/categories', categoryRoutes); // http://localhost:3001/api/categories
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
 
